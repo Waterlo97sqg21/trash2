0d0b18fdbfa672ab03cbb3872d4d9920fdea8ad5
Yuriy Postoronko <ypostoronko@ed3p.com>
2023-02-05 16:57:14 +0200

    edit 3

--- my-project/str.py
+++ my-project/str.py
@@ -1,9 +1,9 @@
 print("Firstly when we use input(),our variable will string type")
-print("Enter two digital,please!")
+print("Enter two digital,please! ")
 a = input()
 b = input()
 s = a + b
-print(s)
+print('Summa: ' , s)
 print("NEXT...")
 first = 5
 second = '7'
@@ -12,8 +12,8 @@ first = '5'
 second = '7'
 print(int(first) * int(second))
 print("NEXT")
-print("Enter two digital")
-q = int(input())
+print("Enter two digital')
+q = int(input());
 w = int(input())
 s = q + w
 # print("suma q+w=", q, "+", w, "=", s)
